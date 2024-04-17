72ee0a4ba0f2dba411f8b0759d16c9750a43207c
Aidarkg <adijamgyrchiev@gmail.com>
2024-04-17 15:36:10 +0600

    video convert

--- audio/views.py
+++ audio/views.py
@@ -1,24 +1,49 @@
-import speech_recognition as sr
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from decouple import config
+from moviepy.editor import VideoFileClip
+import requests
+import os
+
 
 class RecordSpeechAPIView(APIView):
     def post(self, request):
-        recognizer = sr.Recognizer()
-        
-        try:
-            # Получаем аудиофайл из запроса
-            audio_data = request.FILES['audio_file']
-            with sr.AudioFile(audio_data) as source:
-                audio = recognizer.record(source)
-
-            # Распознаем речь с помощью Google Speech Recognition
-            text = recognizer.recognize_google(audio, language="ru-RU")
-            return Response({"text": text}, status=status.HTTP_200_OK)
-        except KeyError:
-            return Response({"error": "No audio file provided"}, status=status.HTTP_400_BAD_REQUEST)
-        except sr.UnknownValueError:
-            return Response({"error": "Unable to recognize speech"}, status=status.HTTP_400_BAD_REQUEST)
-        except sr.RequestError as e:
-            return Response({"error": "Speech recognition service request failed: {}".format(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        url = 'https://asr.ulut.kg/api/receive_data'
+        token = config('TOKEN')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        if 'audio' in request.FILES:
+            audio_file = request.FILES['audio']
+            
+            try:
+                response = requests.post(url, files={'audio': audio_file}, headers=headers)
+                if response.status_code == 200:
+                    data = response.json()
+                    return Response(data, status=status.HTTP_200_OK)
+                else:
+                    return Response({"ошибка": "Не удалось обработать звук"}, status=response.status_code)
+            except Exception as e:
+                return Response({'Ошибка': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif 'video' in request.FILES:
+            video_file = request.FILES['video'].name
+            clip = VideoFileClip(video_file)
+            audio_file = clip.audio
+            video_filename = video_file.split('.')[0]
+            audio_filename = f'media/{video_filename}.mp3'
+            audio_file.write_audiofile(audio_filename)
+            
+            try:
+                with open(audio_filename, 'rb') as audio_data:
+                    response = requests.post(url, files={'audio': audio_data}, headers=headers)
+                    if response.status_code == 200:
+                        data = response.json()
+                        return Response(data, status=status.HTTP_200_OK)
+                    else:
+                        return Response({"ошибка": "Не удалось обработать звук"}, status=response.status_code)
+            except Exception as e:
+                return Response({'Ошибка': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            finally:
+                # Удаляем временный файл аудио после использования
+                os.remove(audio_filename)
